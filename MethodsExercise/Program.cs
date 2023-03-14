970eb99b3af5dcb7d2db5e8e987f72853bf2de4b
yosefamiami@gmail.com <yosefamiami@gmail.com>
2023-03-14 15:24:40 -0400

    finished profile configuration

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, today {band} will be playing at Rogers Park. You can bring {animal} with you. Oh, remember to where a {color} sweater! See ya!");
         }
     }
 }
